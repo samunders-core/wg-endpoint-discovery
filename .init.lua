6cb3c15bde2bffb982da4bfd304e5dbfa9871f46
Samunders Core <sam_@herkales>
2024-03-29 15:15:12 +0100

    /sse logs

--- .init.lua
+++ .init.lua
@@ -7,8 +7,22 @@ local unix = require "unix"
 -- this address is the only configuration "knob"; port can be specified as second argument. But that's it
 manager_address = ParseIp(arg[1] or "")
 
+NEXT = 0
+MESSAGES = 1 * 8
+MAX_MESSAGES = 128
+MESSAGE_SIZE = 512
+log_buffer = unix.mapshared(MESSAGES + MAX_MESSAGES * MESSAGE_SIZE)
+
+function log(level, msg)
+  Log(level, msg)
+  local next = log_buffer:load(NEXT)
+  log_buffer:write(MESSAGES + next * MESSAGE_SIZE, EncodeJson({event=tostring(level), data="<li>%s</li>" % {msg}}))
+  log_buffer:store(NEXT, (next + 1) % MAX_MESSAGES)
+  log_buffer:wake(NEXT)
+end
+
 function system(cmd)
-  Log(kLogInfo, "executing %s: %s" % {cmd, inspect(table.pack(os.execute(cmd)), {newline="", indent="  "})})
+  log(kLogInfo, "executing %s: %s" % {cmd, inspect(table.pack(os.execute(cmd)), {newline="", indent="  "})})
 end
 
 function pid_dir()
@@ -27,7 +41,7 @@ end
 function which(name)
   local path, msg = unix.commandv(GetHostOs() == "WINDOWS" and ("%s.exe" % {name}) or name)
   if msg then
-    Log(kLogFatal, "%s lookup failed: %s" % {name, msg})
+    log(kLogFatal, "%s lookup failed: %s" % {name, msg})
     unix.exit(1)
   elseif GetHostOs() == "WINDOWS" and path:find(" ") then
     path = "\"%s\"" % {path}
@@ -39,11 +53,11 @@ if manager_address == -1 then
   if GetHostOs() == "WINDOWS" and arg[1] == "terminate" then
     system("%s advfirewall firewall delete rule name=redbean_ping" % {which("netsh")})
     pid = Slurp("%s/redbean.pid" % {pid_dir()}) or "-1"
-    Log(kLogWarn, "Sending SIGINT to %s" % {pid})
+    log(kLogWarn, "Sending SIGINT to %s" % {pid})
     unix.kill(tonumber(pid, 10), unix.SIGINT)
     unix.exit(0)
   end
-  Log(kLogFatal, "Malformed manager address provided as first argument: %s" % {arg[1] or ""})
+  log(kLogFatal, "Malformed manager address provided as first argument: %s" % {arg[1] or ""})
   unix.exit(1)
 elseif IsDaemon() and GetHostOs() == "WINDOWS" then  -- so it is possible to turn off first redbean by starting second instance with 'terminate' argument
   ProgramPidPath("%s/redbean.pid" % {pid_dir()})
@@ -56,10 +70,48 @@ NO_SP = "[^[:space:]]+"
 SP = "[[:space:]]+"
 wg_show_pattern = re.compile("(%s)%s(%s)%s(%s)%s(%s)%s(%s)(%s%s%s(%s)%s.*)?" % {NO_SP, SP, NO_SP, SP, NO_SP, SP, NO_SP, SP, NO_SP, SP, NO_SP, SP, NO_SP, SP})
 
+function logread(r)
+  previous_next = r.session.previous_next or 0
+  local errno = select(2, log_buffer:wait(NEXT, previous_next))
+  local next = log_buffer:load(NEXT)
+  if previous_next == next then
+    return "", errno
+  elseif previous_next < next then
+    messages = log_buffer:read(MESSAGES + previous_next * MESSAGE_SIZE, (next - previous_next) * MESSAGE_SIZE)
+  else
+    messages = log_buffer:read(MESSAGES + previous_next * MESSAGE_SIZE, (MAX_MESSAGES - previous_next) * MESSAGE_SIZE)
+    messages = messages .. log_buffer:read(MESSAGES, next * MESSAGE_SIZE)
+  end
+  r.session.previous_next = next
+  return messages, errno
+end
+
+function serve_sse(r)
+  if r.session.sse == "done" then
+    r.session.sse = nil
+    fm.logInfo("Stop SSE processing")
+    return fm.serveContent("sse", {})
+  end
+  r.session.sse = "done"
+  fm.streamContent("sse", {retry=5000})
+  repeat
+    local messages, errno = logread(r)
+    for msg in messages:gmatch("([^\0]+)\0+") do
+      local json, err = DecodeJson(msg)
+      if err then
+        Log(kLogWarn, "Failed to decode '%s': '%s'" % {inspect(msg), err})
+      elseif json then
+        fm.streamContent("sse", json)
+      end
+    end
+  until errno and errno:errno() == unix.EINTR
+  return fm.serveContent("sse", {})
+end
+
 function serve_peers(r)
   local fd, msg = io.popen(wg_show_all_dump, "r")
   if msg then
-    Log(kLogWarn, "%s failed: %s" % {cmd, msg})
+    log(kLogWarn, "%s failed: %s" % {cmd, msg})
     return fm.serveError(500, msg)()
   end
   SetHeader("Content-Type", "text/plain")
@@ -76,7 +128,7 @@ end
 function serve_endpoint(r)
   local fd, msg = io.popen(wg_show_all_dump, "r")
   if msg then
-    Log(kLogWarn, "%s failed: %s" % {cmd, msg})
+    log(kLogWarn, "%s failed: %s" % {cmd, msg})
     return fm.serveError(500, msg)()
   end
   for line in fd:lines() do
@@ -107,29 +159,30 @@ function fetch_endpoint(network, pubkey, endpoint)
   local url = fm.makeUrl("", {scheme="http", host=FormatIp(manager_address), port=arg[2] or "8080", path=fm.makePath("/endpoint/*pubkey", {pubkey=pubkey})})
   local status, headers, body = Fetch(url)
   if body and endpoint and endpoint == body:sub(1, #endpoint) then
-    Log(kLogInfo, "Peer %s still at %s" % {pubkey, endpoint})
+    log(kLogInfo, "Peer %s still at %s" % {pubkey, endpoint})
     local allowed_ips = body:gsub(".* ([0-9.]+)/32", "%1")
     if headers["X-Client-Address"] ~= allowed_ips then
       return allowed_ips
     end
   elseif status == 200 and body and not body:find("(none)") then
-    system("%s set %s peer %s persistent-keepalive 13 endpoint %s" % {wg, network, pubkey, body})
     local allowed_ips = body:gsub(".* ([0-9.]+)/32", "%1")
     if headers["X-Client-Address"] == allowed_ips then
     elseif GetHostOs() == "WINDOWS" then
+      system("%s set %s peer %s persistent-keepalive 13 endpoint %s" % {wg, network, pubkey, body})
       system("%s add %s mask 255.255.255.255 %s" % {which("route"), allowed_ips, headers["X-Client-Address"]})
       system("%s advfirewall firewall add rule name=redbean_ping protocol=icmpv4:8,any dir=in localip=%s action=allow" % {which("netsh"), headers["X-Client-Address"]})
       return allowed_ips
     else
       for _, adapter in ipairs(unix.siocgifconf()) do
         if headers["X-Client-Address"] == FormatIp(adapter.ip) then
+          system("%s set %s peer %s persistent-keepalive 13 endpoint %s" % {wg, network, pubkey, body})
           system("%s route replace %s dev %s scope link" % {which("ip"), allowed_ips, adapter.name})
           return allowed_ips
         end
       end
     end
   elseif status ~= 404 then
-    Log(kLogWarn, "Fetch(%s) failed: %s, %s, %s" % {url, status or "none", headers or "", body or ""})
+    log(kLogWarn, "Fetch(%s) failed: %s, %s, %s" % {url, status or "none", headers or "", body or ""})
   end
   return ""
 end
@@ -160,12 +213,12 @@ function every_minute()
       fd:close()
       return ping(ping_targets)
     end
-    Log(kLogWarn, "%s failed: %s" % {cmd, msg})
+    log(kLogWarn, "%s failed: %s" % {cmd, msg})
   end
-  Log(kLogWarn, "Fetch(%s) failed: %s, %s, %s" % {url, status or "none", error or "", body or ""})
+  log(kLogWarn, "Fetch(%s) failed: %s, %s, %s" % {url, status or "none", error or "", body or ""})
   local fd, msg = io.popen(wg_show_all_dump, "r")
   if msg then
-    return Log(kLogWarn, "%s failed: %s" % {cmd, msg})
+    return log(kLogWarn, "%s failed: %s" % {cmd, msg})
   end
   local ping_targets = {}
   for line in fd:lines() do
@@ -191,6 +244,17 @@ end
 if every_minute then
   fm.setSchedule("* * * * *", every_minute)
 end
+
+fm.setRoute("/sse", serve_sse)
+fm.setRoute("/*", function() return [[
+<!DOCTYPE html><html><head>
+<script src="https://unpkg.com/htmx.org@1.9.11" ></script>
+<script src="https://unpkg.com/htmx.org@1.9.11/dist/ext/sse.js"></script>
+</head>
+<body><h1>Log</h1>
+<ul id="sse" hx-ext="sse" sse-connect="/sse" sse-swap="message,0,1,2,3,4,5,6" hx-swap="beforeend">
+</ul></body></html>]]
+end)
 
 fm.run()
 
