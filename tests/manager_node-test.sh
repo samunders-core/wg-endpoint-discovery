78cd22fd08fa5b58fd03dcbba65236151333f38c
Samunders Core <sam_@herkales>
2024-11-09 11:29:18 +0100

    peers-test.sh

--- tests/manager_node-test.sh
+++ tests/manager_node-test.sh
@@ -15,7 +15,7 @@ after() {
     rm -f ape chmod mkdir mv wg
 }
 
-it_requires_central_node_address_as_argument() {
+it_requires_its_address_as_argument() {
     ! OUTPUT="$(timeout 3 ./redbean.com < /dev/null 2>&1)"
     echo "$OUTPUT" | lookup "Malformed manager address provided as first argument: "
 }
@@ -33,7 +33,7 @@ it_excludes_itself_from_served_online_peers() {
     (
         echo '#!/bin/sh'
         echo '[ ! "$*" = "show all dump" ] && echo "Not implemented" >&2 && exit 1'
-        echo "echo 'vpn1	private_key	central_node_public_key	1234	off'"
+        echo "echo 'vpn1	private_key	manager_node_public_key	1234	off'"
         echo "echo 'vpn1	offline_peer_public_key	(none)	(none)	10.10.10.1/32	42	0	0	13'"
         echo "echo 'vpn1	online_peer_public_key	(none)	9.8.7.6:1234	10.10.10.2/32	1731099015	9351784	3698984	13'"
     ) > wg
@@ -48,14 +48,14 @@ it_serves_endpoint() {
     (
         echo '#!/bin/sh'
         echo '[ ! "$*" = "show all dump" ] && echo "Not implemented" >&2 && exit 1'
-        echo "echo 'vpn1	private_key	central_node_public_key	1234	off'"
+        echo "echo 'vpn1	private_key	manager_node_public_key	1234	off'"
         echo "echo 'vpn1	offline_peer_public_key	(none)	(none)	10.10.10.1/32	42	0	0	13'"
         echo "echo 'vpn1	online_peer_public_key	(none)	9.8.7.6:1234	10.10.10.2/32	1731099015	9351784	3698984	13'"
     ) > wg
     chmod +x wg
     PATH=$(pwd) ./redbean.com 127.0.0.1 &
     sleep 1
-    OUTPUT="$(curl http://localhost:8080/endpoint/central_node_public_key)"
+    OUTPUT="$(curl http://localhost:8080/endpoint/manager_node_public_key)"
     [ "$OUTPUT" = "$(ip -o route get 1.1.1.1 | awk '{print $7}'):1234" ]
     OUTPUT="$(curl http://localhost:8080/endpoint/offline_peer_public_key)"
     echo "$OUTPUT" | lookup "404 Peer not seen yet"
