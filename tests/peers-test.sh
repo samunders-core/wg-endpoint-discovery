7f4f5d49ea8bed03ee371f96300e345c931962a7
Samunders Core <sam_@herkales>
2024-12-07 13:04:56 +0100

    strace also peer

--- tests/peers-test.sh
+++ tests/peers-test.sh
@@ -35,7 +35,7 @@ mock_manager_and_online_peer() {
         echo "echo 'vpn1	online_peer_public_key	(none)	9.8.7.6:1234	127.0.0.1/32	1731099015	9351784	3698984	13'"
     ) > manager/wg  # for test purposes allowed ips of online peer points to manager
     chmod +x manager/wg
-    PATH="$(pwd)/manager" ./redbean.com -X --strace -l 127.0.0.1 -l 127.0.0.27 127.0.0.1 2>&1 | sed 's/^/MANAGER /' &
+    PATH="$(pwd)/manager" ./redbean.com -X --strace -l 127.0.0.1 -l 127.0.0.27 127.0.0.1 2>&1 | sed -re 's/^/MANAGER /' &
     sleep 1
     kill -0 $!
 } 
@@ -52,7 +52,7 @@ mock_wg_show_interfaces() {
 it_pings_peers_on_heartbeat() { 
     mock_manager_and_online_peer 
     mock_wg_show_interfaces
-    OUTPUT="$(timeout 3 env PATH="$(pwd)" ./redbean.com -X -p 9090 127.0.0.27 8080 1000 2>&1 | sed 's/^/PEER /')"
+    OUTPUT="$(timeout 3 env PATH="$(pwd)" ./redbean.com -X --strace -p 9090 127.0.0.27 8080 1000 2>&1 | sed -re 's/^/PEER /')"
     # assert ping
     false
 }
